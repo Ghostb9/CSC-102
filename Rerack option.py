f57bc9c224161643b73c0d30fc30abed7d6f3986
Ghostb9 <bryton.montgomery@spartans.ut.edu>
2023-04-18 22:25:52 -0400

    Update Rerack option.py

--- Rerack option.py
+++ Rerack option.py
@@ -1,4 +1,14 @@
 import tkinter as tk
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO_PIR= 17
+GPIO.setup(GPIO_PIR, GPIO.IN)
+GPIO.setup(16, GPIO.OUT)
+pwm = GPIO.PWM(16, 25)
+x=1
+time.sleep(5)
 
 def go_to_first():
     f1.pack()
@@ -12,8 +22,9 @@ def triangle():
     exit()
 def diamond():
     exit()
-    
+
 master = tk.Tk()
+
 #STARTING PAGE
 f1 = tk.Frame(master)
 l1 = tk.Label(f1, text="")
@@ -37,3 +48,17 @@ master.geometry("320x200")
 f1.pack()
 
 master.mainloop()
+
+while x == 1:
+    if GPIO.input(GPIO_PIR):
+        print ("motion detected")
+        pwm.start(0)
+        duty = 2
+        while duty <=12:
+            pwm.ChangeDutyCycle(duty)
+            time.sleep(1)
+            duty= duty +1
+        pwm.start(0)
+        x += 1
+    else:
+        print("ready")
